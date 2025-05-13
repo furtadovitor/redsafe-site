9d1f068f5efacef9167ebc34af66699b36e878ca
vitor.furtado <vitor.furtado@fiocruz.br>
2025-05-12 22:59:12 -0300

    2 commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Exemplo de animação ao rolar (pode adicionar AOS.js depois)
+document.addEventListener('DOMContentLoaded', function () {
+    // animações aqui se quiser
+  });
+  
---
+++ assets/js/scripts.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Efeitos podem ser adicionados aqui
+});
