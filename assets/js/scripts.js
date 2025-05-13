a673b38d3f4e133bc300dc33f0357b01f27cbf76
vitor.furtado <vitor.furtado@fiocruz.br>
2025-05-13 00:24:03 -0300

    segundo commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Efeitos podem ser adicionados aqui
-});
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 3 }
+    }
+  });
+
+  
